56fdd5038208b6b80ba9dcde549c734c0718587e
padomay1352 <padomay1352@gmail.com>
2021-09-21 23:12:43 +0900

    setting
     - second_node_init.bash 오류수정

--- script/second_node_init.bash
+++ script/second_node_init.bash
@@ -7,7 +7,8 @@
 APP="blockchain"
 APP_DAEMON="blockchaind"
 APP_HOME=$HOME/.$APP
-FIRST_NODE_REST_API="18.118.252.151:26657"
+FIRST_NODE_REST_API="18.118.252.151"
+
 # go install
 sudo rm -rf /usr/local/go
 wget https://golang.org/dl/go1.17.linux-amd64.tar.gz    
@@ -26,7 +27,7 @@ mkdir $GOPATH/pkg
 make
 $APP_DAEMON init second-node --home=$APP_HOME
 sudo yum -y install jq
-wget -qO- 0.0.0.0:26657/genesis | jq -r .result.genesis > $APP_HOME/config/genesis.json
-NODE_ID=$(wget -qO- $FIRST_NODE_REST_API/status | jq -r .result.node_info.id)
-sed "s#persistent_peers = \"\"#persistent_peers = \"$NODE_ID@$FIRST_NODE_REST_API\"#" -i $APP_HOME/config/config.toml
+wget -qO- $FIRST_NODE_REST_API:26657/genesis | jq -r .result.genesis > $APP_HOME/config/genesis.json
+NODE_ID=$(wget -qO- $FIRST_NODE_REST_API:26657/status | jq -r .result.node_info.id)
+sed "s#persistent_peers = \"\"#persistent_peers = \"$NODE_ID@$FIRST_NODE_REST_API:26656\"#" -i $APP_HOME/config/config.toml
 $APP_DAEMON start
